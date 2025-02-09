51fb7119cff71619ba4f4fb0f11623c079878736
pixelomer <37103802+pixelomer@users.noreply.github.com>
2025-02-09 14:26:21 +0300

    Remove margins in licenses window

--- ShijimaLicensesDialog.cc
+++ ShijimaLicensesDialog.cc
@@ -2,13 +2,17 @@
 #include <QVBoxLayout>
 #include <QWidget>
 #include "licenses_generated.hpp"
+#include <QMargins>
 
 ShijimaLicensesDialog::ShijimaLicensesDialog(QWidget *parent): QDialog(parent) {
     auto windowLayout = new QVBoxLayout { this };
+    m_textEdit.setParent(this);
     m_textEdit.setReadOnly(true);
     m_textEdit.setPlainText(QString::fromStdString(shijima_licenses));
     setMinimumWidth(480);
+    setMinimumHeight(480);
     setWindowTitle("Licenses");
     setLayout(windowLayout);
+    windowLayout->setContentsMargins(QMargins {});
     windowLayout->addWidget(&m_textEdit);
 }
