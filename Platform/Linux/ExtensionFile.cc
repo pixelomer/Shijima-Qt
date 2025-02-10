pixelomer/Shijima-Qt

commit 9f58544c7532558ac0463958b506ee9f3dbdfa30

Simplify Linux window backends

Platform/Linux/ExtensionFile.cc
@@ -1,0 +1,28 @@
+#include "ExtensionFile.hpp"
+
+namespace Platform {
+
+ExtensionFile::ExtensionFile(): m_valid(false) {}
+
+ExtensionFile::ExtensionFile(QString const& name, const char *data,
+    size_t len): m_valid(true)
+{
+    if (!m_dir.isValid()) {
+        throw std::runtime_error("failed to create temporary directory for GNOME extension");
+    }
+    m_path = m_dir.filePath(name);
+    QFile file { m_path };
+    if (!file.open(QFile::WriteOnly)) {
+        throw std::runtime_error("failed to create temporary file for KDE extension");
+    }
+    QDataStream stream { &file };
+    stream << QByteArray(data, len);
+    file.flush();
+    file.close();
+}
+
+QString const& ExtensionFile::path() {
+    return m_path;
+}
+
+}

Platform/Linux/ExtensionFile.hpp
@@ -1,0 +1,20 @@
+#pragma once
+
+#include <QTemporaryDir>
+#include <QString>
+
+namespace Platform {
+
+class ExtensionFile {
+private:
+    QTemporaryDir m_dir;
+    QString m_path;
+    bool m_valid;
+public:
+    ExtensionFile();
+    ExtensionFile(QString const& filename, const char *data, size_t len);
+    QString const& path();
+    bool valid();
+};
+
+}

Platform/Linux/GNOMEWindowObserverBackend.cc
@@ -10,24 +10,13 @@ namespace Platform {
 const QString GNOMEWindowObserverBackend::m_gnomeScriptUUID = "shijima-helper@pixelomer.github.io";
 const QString GNOMEWindowObserverBackend::m_gnomeScriptVersion = "1.2";
 
-GNOMEWindowObserverBackend::GNOMEWindowObserverBackend():
-    m_gnomeScriptPath(m_gnomeScriptDir.filePath("shijima_gnome_extension.zip"))
+GNOMEWindowObserverBackend::GNOMEWindowObserverBackend(): m_extensionFile(
+    "shijima_gnome_extension.zip", gnome_script, gnome_script_len)
 {
     if (!GNOME::userExtensionsEnabled()) {
         GNOME::setUserExtensionsEnabled(true);
     }
-    if (!m_gnomeScriptDir.isValid()) {
-        throw std::runtime_error("failed to create temporary directory for GNOME extension");
-    }
-    QFile file { m_gnomeScriptPath };
-    if (!file.open(QFile::WriteOnly)) {
-        throw std::runtime_error("failed to create temporary file for KDE extension");
-    }
-    QDataStream stream { &file };
-    stream << QByteArray(gnome_script, gnome_script_len);
-    file.flush();
-    file.close();
-    GNOME::installExtension(m_gnomeScriptPath);
+    GNOME::installExtension(m_extensionFile.path());
     auto extensionInfo = GNOME::getExtensionInfo(m_gnomeScriptUUID);
     static const QString kVersionName = "version-name";
     std::string restartReason;

Platform/Linux/KDEWindowObserverBackend.cc
@@ -10,20 +10,8 @@ namespace Platform {
 const QString KDEWindowObserverBackend::m_kwinScriptName = "ShijimaScript";
 
 KDEWindowObserverBackend::KDEWindowObserverBackend(): WindowObserverBackend(),
-    m_kwinScriptPath(m_kwinScriptDir.filePath("shijima_kwin_script.js"))
+    m_extensionFile("shijima_kwin_script.js", kwin_script, kwin_script_len)
 {
-    if (!m_kwinScriptDir.isValid()) {
-        throw std::runtime_error("failed to create temporary directory for KDE extension");
-    }
-    QFile file { m_kwinScriptPath };
-    if (!file.open(QFile::WriteOnly | QFile::Text)) {
-        throw std::runtime_error("failed to create temporary file for KDE extension");
-    }
-    QTextStream stream { &file };
-    stream << QByteArray(kwin_script, kwin_script_len);
-    stream.flush();
-    file.flush();
-    file.close();
     loadKWinScript();
     startKWinScript();
 }
@@ -44,7 +32,8 @@ void KDEWindowObserverBackend::stopKWinScript() {
 }
 
 void KDEWindowObserverBackend::loadKWinScript() {
-    m_kwinScriptID = KWin::loadScript(m_kwinScriptPath, m_kwinScriptName);
+    m_kwinScriptID = KWin::loadScript(m_extensionFile.path(),
+        m_kwinScriptName);
 }
 
 void KDEWindowObserverBackend::startKWinScript() {

Platform/Linux/GNOMEWindowObserverBackend.hpp
@@ -1,6 +1,7 @@
 #include "WindowObserverBackend.hpp"
 #include <QString>
 #include <QTemporaryDir>
+#include "ExtensionFile.hpp"
 
 namespace Platform {
 
@@ -8,8 +9,7 @@ class GNOMEWindowObserverBackend : public WindowObserverBackend {
 private:
     static const QString m_gnomeScriptUUID;
     static const QString m_gnomeScriptVersion;
-    QTemporaryDir m_gnomeScriptDir;
-    QString m_gnomeScriptPath;
+    ExtensionFile m_extensionFile;
 public:
     explicit GNOMEWindowObserverBackend();
     ~GNOMEWindowObserverBackend();

Platform/Linux/KDEWindowObserverBackend.hpp
@@ -1,14 +1,14 @@
 #include "WindowObserverBackend.hpp"
 #include <QString>
 #include <QTemporaryDir>
+#include "ExtensionFile.hpp"
 
 namespace Platform {
 
 class KDEWindowObserverBackend : public WindowObserverBackend {
 private:
     static const QString m_kwinScriptName;
-    QTemporaryDir m_kwinScriptDir;
-    QString m_kwinScriptPath;
+    ExtensionFile m_extensionFile;
     int m_kwinScriptID = -1;
     void loadKWinScript();
     void startKWinScript();

Platform/Linux/Makefile
@@ -1,7 +1,8 @@
 include ../../common.mk
 
 SOURCES = Platform.cc ActiveWindowObserver.cc KWin.cc PrivateActiveWindowObserver.cc \
-	DBus.cc GNOME.cc GNOMEWindowObserverBackend.cc KDEWindowObserverBackend.cc
+	DBus.cc GNOME.cc GNOMEWindowObserverBackend.cc KDEWindowObserverBackend.cc \
+	ExtensionFile.cc
 QT_LIBS = Core DBus Widgets
 
 define bin2c

commit 0af5a14208fe7e3f7afc9b3a47ff34ce7991c4ff

Fix KDE backend
Regression in 9f58544

Platform/Linux/ExtensionFile.cc
@@ -4,19 +4,30 @@ namespace Platform {
 
 ExtensionFile::ExtensionFile(): m_valid(false) {}
 
-ExtensionFile::ExtensionFile(QString const& name, const char *data,
-    size_t len): m_valid(true)
+ExtensionFile::ExtensionFile(QString const& name, bool text,
+    const char *data, size_t len): m_valid(true)
 {
     if (!m_dir.isValid()) {
         throw std::runtime_error("failed to create temporary directory for GNOME extension");
     }
     m_path = m_dir.filePath(name);
     QFile file { m_path };
-    if (!file.open(QFile::WriteOnly)) {
+    QFile::OpenMode mode = QFile::WriteOnly;
+    if (text) {
+        mode |= QFile::Text;
+    }
+    if (!file.open(mode)) {
         throw std::runtime_error("failed to create temporary file for KDE extension");
     }
-    QDataStream stream { &file };
-    stream << QByteArray(data, len);
+    if (text) {
+        QTextStream stream { &file };
+        stream << QByteArray(data, len);
+        stream.flush();
+    }
+    else {
+        QDataStream stream { &file };
+        stream << QByteArray(data, len);
+    }
     file.flush();
     file.close();
 }

Platform/Linux/ExtensionFile.hpp
@@ -12,7 +12,7 @@ private:
     bool m_valid;
 public:
     ExtensionFile();
-    ExtensionFile(QString const& filename, const char *data, size_t len);
+    ExtensionFile(QString const& filename, bool text, const char *data, size_t len);
     QString const& path();
     bool valid();
 };

Platform/Linux/GNOMEWindowObserverBackend.cc
@@ -11,7 +11,7 @@ const QString GNOMEWindowObserverBackend::m_gnomeScriptUUID = "shijima-helper@pi
 const QString GNOMEWindowObserverBackend::m_gnomeScriptVersion = "1.2";
 
 GNOMEWindowObserverBackend::GNOMEWindowObserverBackend(): m_extensionFile(
-    "shijima_gnome_extension.zip", gnome_script, gnome_script_len)
+    "shijima_gnome_extension.zip", false, gnome_script, gnome_script_len)
 {
     if (!GNOME::userExtensionsEnabled()) {
         GNOME::setUserExtensionsEnabled(true);

Platform/Linux/KDEWindowObserverBackend.cc
@@ -10,7 +10,7 @@ namespace Platform {
 const QString KDEWindowObserverBackend::m_kwinScriptName = "ShijimaScript";
 
 KDEWindowObserverBackend::KDEWindowObserverBackend(): WindowObserverBackend(),
-    m_extensionFile("shijima_kwin_script.js", kwin_script, kwin_script_len)
+    m_extensionFile("shijima_kwin_script.js", true, kwin_script, kwin_script_len)
 {
     loadKWinScript();
     startKWinScript();